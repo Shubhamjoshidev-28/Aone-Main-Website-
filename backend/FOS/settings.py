ced77ea4832f7a52bc7a57093e34fed28480e910
shubhshy <shubhamjoshidev28@gmail.com>
2026-09-02 09:50:06 +0530

    updated the deployement branch

--- backend/FOS/settings.py
+++ backend/FOS/settings.py
@@ -64,10 +64,7 @@ MIDDLEWARE = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-]
+CORS_ALLOWED_ORIGINS = []
 
 ROOT_URLCONF = 'FOS.urls'
 
